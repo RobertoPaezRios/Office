b7b91a502ae4e144fc9dffaef97905454c73afa3
RobertoPaezRios <robepa2009@gmail.com>
2023-05-25 18:25:45 +0200

    v1.1.3

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -426,8 +426,8 @@
                                             @else
                                                 <x-button class="hover:bg-gray-800" disabled>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                                    </svg>
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                                    </svg>                                                      
                                                 </x-button>
                                             @endif
                                     </td>
