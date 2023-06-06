53f5d42199a9110ba5026c1923903fadbe72beb9
RobertoPaezRios <robepa2009@gmail.com>
2023-06-06 23:26:39 +0200

    v1.1.6.1

--- resources/views/livewire/search-types.blade.php
+++ resources/views/livewire/search-types.blade.php
@@ -445,7 +445,7 @@
                                         </svg>                                                      
                                     </x-button>
                                 @else
-                                    <x-danger-button class="hover:bg-red-700" wire:click="destroy({{$type->id}})">
+                                    <x-danger-button class="hover:bg-red-700" data-bs-toggle="modal" data-bs-target="#confirmateModal">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                         </svg>                                  
@@ -458,6 +458,34 @@
                                     </svg>                                  
                                 </x-danger-button>
                             @endif
+                            
+                            <!-- CONFIRMATE MODAL -->
+                            <div class="modal fade" id="confirmateModal" tabindex="-1" aria-labelledby="confirmateModal" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title text-gray-900 font-bold"
+                                                id="confirmateModalLabel">Confirmate Deleting</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                </svg>                                                                  
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">Are you sure you want to delete this register?, this will be permanent. </div>
+                                        <div class="modal-footer">
+                                            <x-button type="button" data-bs-dismiss="modal">
+                                                Close
+                                            </x-button>
+                                            <x-danger-button type="button">
+                                                <a href="#{{$key}}">
+                                                    Delete
+                                                </a>
+                                            </x-danger-button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                           </th>
                           <th scope="row" class="px-6 py-4">
                             {{$sales[$type->id]['sales']}}
